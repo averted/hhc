b0193e7f1c2dd1ee2b9c1dd50f7af6b1ce357f65
Alexander Vitiuk <vitiuk.alex@gmail.com>
2013-09-13 15:38:41 +1000

    updated index with autoloading

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+use hhc\DB\Hero;
+use hhc\DB\HeroQuery;
+use hhc\DB\User;
+use hhc\DB\UserQuery;
+use hhc\DB\Votes;
+use hhc\DB\VotesQuery;
+use hhc\DB\UserVotes;
+use hhc\DB\UserVotesQuery;
+
+require __DIR__.'/autoload.php';
+
 // hon-hero-counter
 
 ?>

0cc4233c540e16ac042f1d814f295fbaada0b9a6
Alexander Vitiuk <vitiuk.alex@gmail.com>
2013-09-14 13:01:36 +1000

    added more templates and additional routes

--- webroot/index.php
+++ webroot/index.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 require dirname(__DIR__).'/autoload.php';
+require dirname(__DIR__).'/controllers/helpers.php';
 
 /**
  * ---------------------
@@ -49,6 +50,19 @@ $app->get('/', function() use($app) {
 
 /**
  * ----------------------
+ * route /load
+ * ----------------------
+ */
+$app->get('/load', function() use($app) {
+    include_once __DIR__.'/../load-heroes.php';
+    return $app['twig']->render('index.html.twig', array(
+        'link' => '/hero',
+        'message' => 'Everything loaded fine.'
+    ));
+});
+
+/**
+ * ----------------------
  * route /hero
  * ----------------------
  */
@@ -57,6 +71,208 @@ $app->get('/hero', function() use ($app) {
     
     return $app['twig']->render('hero-list.html.twig', array(
         'heroes' => $heroes
+    ));
+});
+
+$app->get('/hero/{name}', function($name) use ($app) {
+    $user = $app['session']->get('user');
+
+    if (!valid($name))
+        return $app->redirect('/hero');
+
+    $url = array(
+        'new' => isLoggedIn($user) ? '/counter/'.$name : '/login',
+    );
+
+    $hero = HeroQuery::create()->filterByName(deslug($name))->findOne();
+    $hero->setName($name);
+    $votes = VotesQuery::create()->filterByHeroName(deslug($name))->orderByVotes('desc')->limit(3)->find();
+
+    foreach ($votes as $counter) {
+        $counters[] = array(
+            'name'     => slug($counter->getCounterName()),
+            'votes'    => $counter->getVotes(),
+            'voteup'   => isLoggedIn($user) ? '/hero/'.$name.'/counter/'.slug($counter->getCounterName()).'/voteup' : '/login',
+            'votedown' => isLoggedIn($user) ? '/hero/'.$name.'/counter/'.slug($counter->getCounterName()).'/votedown' : '/login',
+        );
+    }
+    
+    return $app['twig']->render('hero.html.twig', array(
+        'url' => $url,
+        'hero' => $hero,
+        'counters' => $counters
+    ));
+});
+
+$app->get('/hero/{name}/counter/{counter}/voteup', function($name, $counter) use ($app) {
+    $user = $app['session']->get('user');
+
+    if (!valid($name) || !valid($counter))
+        return $app->redirect('/hero');
+
+    if (isLoggedIn($user)) { 
+        if (!voted($user, $name, $counter)) {
+            // increate total votes
+            $votes = VotesQuery::create()->filterByHeroName(deslug($name))->filterByCounterName(deslug($counter))->findOne();
+            $votes->setVotes($votes->getVotes() + 1);
+            $votes->save();
+            
+            registerVote($user, $name, $counter);
+        } else {
+            return 'You may only vote once';
+        }
+    }
+    
+    return $app->redirect('/hero/'.$name);
+});
+
+$app->get('/hero/{name}/counter/{counter}/votedown', function($name, $counter) use ($app) {
+    $user = $app['session']->get('user');
+
+    if (!valid($name) || !valid($counter))
+        return $app->redirect('/hero');
+
+    if (isLoggedIn($user)) {
+        if (!voted($user, $name, $counter)) {
+            // decreate total votes
+            $votes = VotesQuery::create()->filterByHeroName(deslug($name))->filterByCounterName(deslug($counter))->findOne();
+            $votes->setVotes($votes->getVotes() - 1);
+            $votes->save();
+
+            registerVote($user, $name, $counter);
+        } else {
+            return 'You may only vote once';
+        }
+    }
+    
+    return $app->redirect('/hero/'.$name);
+});
+
+/**
+ * ----------------------
+ * route /counter
+ * ----------------------
+ */
+$app->get('/counter/{name}', function($name) use ($app) {
+    if (!valid($name))
+        return $app->redirect('/hero');
+
+    $hero   = HeroQuery::create()->filterByName(deslug($name))->findOne();
+    $heroes = HeroQuery::create()->orderByName()->find();
+    $votes  = VotesQuery::create()->filterByHeroName(deslug($name))->find();
+    
+    foreach ($votes as $index => $counter) {
+        $counters[$index] = $counter->getCounterName();
+    }
+
+    return $app['twig']->render('counter-list.html.twig', array(
+        'hero' => $hero,
+        'heroes' => $heroes,
+        'counters' => $counters
+    ));
+});
+
+$app->get('/counter/{name}/add/{counter}', function($name, $counter) use ($app) {
+    if (!valid($name) || !valid($counter))
+        return $app->redirect('/hero');
+
+    if (VotesQuery::create()->filterByHeroName(deslug($name))->filterByCounterName(deslug($counter))->find()->count() != 0) // counter already exists
+        return $app->redirect('/hero/'.$name.'/counter/'.$counter.'/voteup');
+        
+
+    $hero = HeroQuery::create()->filterByName(deslug($name))->findOne();
+
+    $votes = new Votes();
+    $votes->setHeroName(deslug($name));
+    $votes->setCounterName(deslug($counter));
+    $votes->setVotes(1);
+    $votes->save();
+
+    return $app->redirect('/hero/'.$name);
+});
+
+/**
+ * ----------------------
+ * route /login
+ * ----------------------
+ */
+$app->match('/login', function (Request $request) use ($app) {
+    $user  = $app['session']->get('user');
+    $error = null;
+
+    // check if user is logged in
+    if (isLoggedIn($user))
+        return $app->redirect('/');
+       
+    if ($request->getMethod() == 'POST') {
+        if (!($username = $request->get('_username')))
+            $error = "Username is required";
+        else if (!($password = $request->get('_password')))
+            $error = "Password is required";
+        else if (UserQuery::create()->filterByUsername($username)->find()->count() == 0)
+            $error = "Username not found";
+        
+        if (!$error) {
+            $user = UserQuery::create()->filterByUsername($username)->findOne();
+            
+            if ($username == $user->getUsername() && $password == $user->getPassword()) {
+                $app['session']->set('user', $username);
+                return $app->redirect('/');
+            } else {
+                $error = 'Bad credentials';
+            }
+        }
+    }
+
+    return $app['twig']->render('login.html.twig', array(
+        'error' => $error
+    ));
+});
+
+/**
+ * ----------------------
+ * route /register
+ * ----------------------
+ */
+$app->match('/register', function (Request $request) use ($app) {
+    $user  = $app['session']->get('user');
+    $error = null;
+
+    // check if user is logged in
+    if (isLoggedIn($user))
+        return $app->redirect('/');
+
+    if ($request->getMethod() == 'POST') {
+        if (!($email = $request->get('_email')))
+            $error = "Email is required";
+        else if (!($username = $request->get('_username')))
+            $error = "Username is required";
+        else if (!($password = $request->get('_password')))
+            $error = "Password is required";
+        else if (!($password2 = $request->get('_password2')))
+            $error = "Repeating your password is required";
+        else if ($password != $password2)
+            $error = "Passwords don't match";
+
+        if (!$error) {
+            $user = new User();
+            $user->setEmail($email);
+            $user->setUsername($username);
+            $user->setPassword($password);
+            
+            if ($user->validate()) {
+                $user->save();
+                return $app->redirect('/');
+            } else {
+                foreach ($user->getValidationFailures() as $failure) {
+                    $error = $failure->getMessage();
+                }
+            }
+        }
+    }
+
+    return $app['twig']->render('register.html.twig', array(
+        'error' => $error
     ));
 });
 
